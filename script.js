b756067958e14f7e0b2394aeca3e9670ef68a8bb
Karl <karlemerykoffi@gmail.com>
2026-05-10 02:25:49 +0000

    add random number

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ console.log(
 )
 const randomNumberEL = document.querySelector("#random-number");
 randomNumberEL.addEventListener("click" , () =>{
-    document.querySelector("#result").textContent = Math.random();
+    document.querySelector("#result").textContent = Math.floor(
+     getRandomNumber(1, 100)   
+    );
 });
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+};
