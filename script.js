8595a4bfe6093458546b574827aa90e9a1aea062
Karl <karlemerykoffi@gmail.com>
2026-05-10 02:17:15 +0000

    feat : add random botton

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log(
     "hey"
 )
+const randomNumberEL = document.querySelector("#random-number");
+randomNumberEL.addEventListener("click" , () =>{
+    document.querySelector("#result").textContent = Math.random();
+});
